67adea0a3c3cc4625f4d7fcac07670e7289a1a5f
smithb1994 <14811263+smithb1994@users.noreply.github.com>
2023-12-01 13:31:15 -0500

    - updated db

--- database/db_models.js
+++ database/db_models.js
@@ -1,35 +1,47 @@
-const lib = require('../lib/index.js');
-const knex = lib.config.db;
+require('dotenv').config();
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: process.env['DATABASE_URL'],
+  pool: {max: 10},
+  debug: false
+});
 
 //===== MODELS =====
 Promise.resolve()
-//TODO: settings
-//TODO: allocation
-
-//CHECK: performance
+//CHECK: bots
 .then(() => {
-  return knex.schema.createTable('performance', (table) => {
-    table.string('performance_id', 50).notNullable().primary().unique();
-    table.string('strategy_id', 50).notNullable().references('strategies.strategy_id').onDelete('CASCADE').index();
-    table.decimal('strategy_value', 30, 15).notNullable();
-    table.jsonb('industries').notNullable().defaultTo('{}');
-    table.decimal('gain_hour', 30, 15).notNullable().defaultTo(0);
-    table.decimal('gain_day', 30, 15).notNullable().defaultTo(0);
-    table.decimal('gain_week', 30, 15).notNullable().defaultTo(0);
-    table.decimal('gain_month', 30, 15).notNullable().defaultTo(0);
-    table.decimal('gain_year', 30, 15).notNullable().defaultTo(0);
-    table.decimal('gain_five_year', 30, 15).notNullable().defaultTo(0);
-    table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()')).index();
+  return knex.schema.createTable('bots', (table) => {
+    table.string('bot_id', 50).notNullable().primary().unique();
+    table.bigInteger('buffer_amount').notNullable().defaultTo('0');
+    table.decimal('max_day_margin', 30, 15); //0-4
+    table.decimal('max_night_margin', 30, 15); //0-4
+    table.time('rebalance_day_margin_at');
+    table.time('rebalance_night_margin_at');
+    table.string('error_action', 50).notNullable().defaultTo('close'); //close, stop
+    table.string('alphainsider_key', 100).notNullable();
+    table.jsonb('broker').notNullable().defaultTo('{}'); //{type, account_id, live, alpaca_key, alpaca_secret, tastytrade_email, tastytrade_password}
+    table.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'));
   });
 })
 
-//TODO: timelines
-
-//DONE: errors
+//CHECK: allocation
 .then(() => {
-  return knex.schema.createTable('errors', (table) => {
-    table.string('error_id', 50).notNullable().primary().unique();
-    table.string('type', 50).notNullable().index();
+  return knex.schema.createTable('allocation', (table) => {
+    table.string('allocation_id', 50).notNullable().primary().unique();
+    table.string('bot_id', 50).notNullable().references('bots.bot_id').onDelete('CASCADE').onUpdate('CASCADE');
+    table.string('strategy_id', 50).notNullable();
+    table.decimal('multiplier', 30, 15).notNullable().defaultTo('1');
+    table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+  });
+})
+
+//CHECK: activity
+.then(() => {
+  return knex.schema.createTable('activity', (table) => {
+    table.string('activity_id', 50).notNullable().primary().unique();
+    table.string('bot_id', 50).notNullable().references('bots.bot_id').onDelete('CASCADE').onUpdate('CASCADE');
+    table.string('type', 50).notNullable().index(); //info, warning, error
     table.jsonb('info').notNullable().defaultTo('{}');
     table.string('message', 30000);
     table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()')).index();
@@ -38,7 +50,10 @@ Promise.resolve()
 
 // FINISH
 .then((data) => {
-  console.log('Done!!');
+  console.log('Database created!');
+})
+.catch((error) => {
+  console.log(error);
 })
 // EXIT
 .then(() => {
